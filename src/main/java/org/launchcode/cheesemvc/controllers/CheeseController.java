776b5f09deddc4c8233e1d6f5c333bec0bc978f8
Aaron Pham <aaronp9485@gmail.com>
2017-08-21 18:21:52 -0700

    add form processing

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,7 +3,8 @@ package org.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.thymeleaf.expression.Strings;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -11,17 +12,31 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     // Request path: /cheese
     @RequestMapping(value= "")
     public String index(Model model) {
 
-        ArrayList<Strings> cheeses = new ArrayList<>();
-        cheeses.add("cheddar");
-        cheeses.add("parmesan");
-        cheeses.add("munster");
-
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
+    }
+
+    // Displaying the Form
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    // Process the form - note the processing goes to the same URL as
+    // as the form display by default unless you specify where it should go
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+
+        // Redirect to /cheese
+        return "redirect:";
     }
 }
